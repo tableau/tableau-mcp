4d89fecce7427ffd645bbfaad47708d66f881693
Andy Young <andrew.young@tableau.com>
2025-05-09 12:11:53 -0700

    Move interceptors to instance

--- src/restApiInstance.ts
+++ src/restApiInstance.ts
@@ -1,5 +1,8 @@
+import { config } from './config.js';
+import { log, shouldLogWhenLevelIsAtLeast } from './log.js';
+import { maskRequest, maskResponse } from './logging/secretMask.js';
 import { AuthConfig } from './sdks/tableau/authConfig.js';
-import { getRequestInterceptor, getResponseInterceptor } from './sdks/tableau/interceptors.js';
+import { RequestInterceptor, ResponseInterceptor } from './sdks/tableau/interceptors.js';
 import RestApi from './sdks/tableau/restApi.js';
 
 export const getNewRestApiInstanceAsync = async (
@@ -15,3 +18,44 @@ export const getNewRestApiInstanceAsync = async (
   await restApi.signIn(authConfig);
   return restApi;
 };
+
+export const getRequestInterceptor =
+  (requestId: string): RequestInterceptor =>
+  (request) => {
+    const maskedRequest = config.disableLogMasking ? request : maskRequest(request);
+    const { baseUrl, url } = maskedRequest;
+    const urlParts = [...baseUrl.split('/'), ...url.split('/')].filter(Boolean);
+    const messageObj = {
+      type: 'request',
+      requestId,
+      method: maskedRequest.method,
+      url: urlParts.join('/'),
+      ...(shouldLogWhenLevelIsAtLeast('debug') && {
+        headers: maskedRequest.headers,
+        data: maskedRequest.data,
+      }),
+    } as const;
+
+    log.info(messageObj, 'rest-api');
+    return request;
+  };
+
+export const getResponseInterceptor =
+  (requestId: string): ResponseInterceptor =>
+  (response) => {
+    const maskedResponse = config.disableLogMasking ? response : maskResponse(response);
+    const { baseUrl, url } = maskedResponse;
+    const urlParts = [...baseUrl.split('/'), ...url.split('/')].filter(Boolean);
+    const messageObj = {
+      type: 'response',
+      requestId,
+      url: urlParts.join('/'),
+      status: maskedResponse.status,
+      ...(shouldLogWhenLevelIsAtLeast('debug') && {
+        headers: maskedResponse.headers,
+        data: maskedResponse.data,
+      }),
+    } as const;
+
+    log.info(messageObj, 'rest-api');
+  };
--- src/sdks/tableau/interceptors.ts
+++ src/sdks/tableau/interceptors.ts
@@ -1,8 +1,3 @@
-import { config } from '../../config.js';
-import { log, shouldLogWhenLevelIsAtLeast } from '../../log.js';
-import { maskRequest } from '../../logging/secretMask.js';
-import { maskResponse } from '../../logging/secretMask.js';
-
 export type RequestInterceptorConfig = {
   method: string;
   baseUrl: string;
@@ -21,44 +16,3 @@ export type ResponseInterceptorConfig = {
 
 export type RequestInterceptor = (config: RequestInterceptorConfig) => void;
 export type ResponseInterceptor = (response: ResponseInterceptorConfig) => void;
-
-export const getRequestInterceptor =
-  (requestId: string): RequestInterceptor =>
-  (request) => {
-    const maskedRequest = config.disableLogMasking ? request : maskRequest(request);
-    const { baseUrl, url } = maskedRequest;
-    const urlParts = [...baseUrl.split('/'), ...url.split('/')].filter(Boolean);
-    const messageObj = {
-      type: 'request',
-      requestId,
-      method: maskedRequest.method,
-      url: urlParts.join('/'),
-      ...(shouldLogWhenLevelIsAtLeast('debug') && {
-        headers: maskedRequest.headers,
-        data: maskedRequest.data,
-      }),
-    } as const;
-
-    log.info(messageObj, 'rest-api');
-    return request;
-  };
-
-export const getResponseInterceptor =
-  (requestId: string): ResponseInterceptor =>
-  (response) => {
-    const maskedResponse = config.disableLogMasking ? response : maskResponse(response);
-    const { baseUrl, url } = maskedResponse;
-    const urlParts = [...baseUrl.split('/'), ...url.split('/')].filter(Boolean);
-    const messageObj = {
-      type: 'response',
-      requestId,
-      url: urlParts.join('/'),
-      status: maskedResponse.status,
-      ...(shouldLogWhenLevelIsAtLeast('debug') && {
-        headers: maskedResponse.headers,
-        data: maskedResponse.data,
-      }),
-    } as const;
-
-    log.info(messageObj, 'rest-api');
-  };
