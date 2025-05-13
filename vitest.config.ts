872be5c703a5b2d023634ae72a610be0023e843b
Andy Young <andrew.young@tableau.com>
2025-05-12 17:21:24 -0700

    Exclude SDKs from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config';
+import { coverageConfigDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
@@ -19,6 +19,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       include: ['src'],
+      exclude: ['src/sdks/**/*', ...coverageConfigDefaults.exclude],
       reporter: ['text', 'cobertura'],
       reportsDirectory: './coverage/unit',
     },
