e0bdabb6348d0c903a9828305008a820d50515ec
Renoy John <renoymaratt@gmail.com>
2025-05-21 11:01:49 -0700

    Fixing missed merge conflict

--- src/tools/toolName.ts
+++ src/tools/toolName.ts
@@ -1,17 +1,4 @@
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-export const toolNames = ['list-datasources', 'query-datasource', 'list-fields', 'read-metadata'] as const;
-=======
-export const toolNames = ['list-fields', 'query-datasource', 'read-metadata'] as const;
->>>>>>> main
-=======
-export const toolNames = [
-  'list-datasources',
-  'query-datasource',
-  'list-fields',
-  'read-metadata',
-] as const;
->>>>>>> Stashed changes
+export const toolNames = ['list-datasources', 'list-fields', 'query-datasource', 'read-metadata'] as const;
 export type ToolName = (typeof toolNames)[number];
 
 export function isToolName(value: unknown): value is ToolName {
