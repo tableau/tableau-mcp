1e0f00d8dfa065a97076c1ef43c19e1c3831e6af
Andy Young <andrew.young@tableau.com>
2025-05-28 17:10:18 -0700

    Merge s.montesdeoca/add-filters-hbi-query

--- src/tools/queryDatasource/queryDescription.ts
+++ src/tools/queryDatasource/queryDescription.ts
@@ -1,1 +1,1 @@
-export const queryDatasourceToolDescription = `Run a Tableau VizQL query.`;
+export const queryDatasourceToolDescription = `Run a Tableau VizQL query. You must follow the Query schema from vizqlDataServiceApi.ts!`;
