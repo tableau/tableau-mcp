2bd83a3529a99b07e382d5871d996584eb6d00f2
Andy Young <andrew.young@tableau.com>
2025-07-22 13:07:33 -0700

    Fix error

--- src/server/oauth/provider.ts
+++ src/server/oauth/provider.ts
@@ -347,7 +347,7 @@ export class OAuthProvider {
         redirectUri,
         codeChallenge,
         codeChallengeMethod,
-        state,
+        state: state ?? '',
         scope,
         tableauState,
       });
