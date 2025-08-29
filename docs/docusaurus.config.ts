9cebec5a3b966e21d4eb6d494bd0595e61f7af9d
Andy Young <andrew.young@tableau.com>
2025-08-29 16:18:44 -0700

    Fix baseUrl

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://tableau.github.io/tableau-mcp/',
+  url: 'https://tableau.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tableau-mcp/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
