c8f630e55978a84f0245dfba1f05ace32791ea46
Andy Young <andrew.young@tableau.com>
2025-09-03 17:53:08 -0700

    Fix import

--- e2e/client.ts
+++ e2e/client.ts
@@ -1,4 +1,4 @@
-import { Client } from '@modelcontextprotocol/sdk/client';
+import { Client } from '@modelcontextprotocol/sdk/client/index.js';
 import { StdioClientTransport } from '@modelcontextprotocol/sdk/client/stdio.js';
 import { z } from 'zod';
 
