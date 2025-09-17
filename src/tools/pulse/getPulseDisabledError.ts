881af4c69a45caa11cb8edc15917a6f4e0060e1e
Andy Young <andrew.young@tableau.com>
2025-09-17 14:16:27 -0700

    Fix typo

--- src/tools/pulse/getPulseDisabledError.ts
+++ src/tools/pulse/getPulseDisabledError.ts
@@ -1,7 +1,7 @@
 export function getPulseDisabledError(): string {
   return [
     'Pulse is disabled.',
-    'To enable Pulse on your Tableau Cloud site, see the instuctions at https://help.tableau.com/current/online/en-us/pulse_set_up.htm.',
+    'To enable Pulse on your Tableau Cloud site, see the instructions at https://help.tableau.com/current/online/en-us/pulse_set_up.htm.',
     'Pulse is not available on Tableau Server.',
   ].join(' ');
 }
