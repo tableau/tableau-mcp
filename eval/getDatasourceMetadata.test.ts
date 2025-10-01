968c6fd2db7fdbad75f373b98ce5607d3bea5521
Andy Young <andrew.young@tableau.com>
2025-10-01 16:10:26 -0700

    Split up tests

--- eval/getDatasourceMetadata.test.ts
+++ eval/getDatasourceMetadata.test.ts
@@ -1,10 +1,9 @@
 import { MCPServerStdio, run, withTrace } from '@openai/agents';
 import dotenv from 'dotenv';
-import z from 'zod';
 
 import { Datasource } from '../e2e/constants.js';
 import { getDefaultEnv, getSuperstoreDatasource, resetEnv, setEnv } from '../e2e/testEnv.js';
-import { dataSourceSchema } from '../src/sdks/tableau/types/dataSource.js';
+import { fieldsResultSchema } from '../src/tools/getDatasourceMetadata/datasourceMetadataUtils.js';
 import {
   getAgentWithTools,
   getApiKey,
@@ -16,7 +15,7 @@ import {
   validateCertChain,
 } from './base.js';
 
-describe('list-datasources', () => {
+describe('get-datasource-metadata', () => {
   let model: string;
   let mcpServer: MCPServerStdio;
   let superstore: Datasource;
@@ -41,9 +40,8 @@ describe('list-datasources', () => {
     await mcpServer.close();
   });
 
-  it('should call list_datasources tool', async () => {
-    const message =
-      'List the data sources that are available on my Tableau site. Do not perform any analysis on the the list of data sources, just show the list.';
+  it('should call get_datasource_metadata tool', async () => {
+    const message = `For the Superstore data source, get its metadata. The data source luid for the Superstore data source is ${superstore.id}. Do not perform any analysis on the metadata, just show it.`;
     log(`Running: ${message}`, true);
 
     const agent = await getAgentWithTools(mcpServer, model);
@@ -59,20 +57,16 @@ describe('list-datasources', () => {
     });
 
     const toolExecutions = await getToolExecutions(result);
-
     expect(toolExecutions.length).toBe(1);
-    expect(toolExecutions[0].name).toBe('list_datasources');
-    expect(toolExecutions[0].arguments.filter).toBeFalsy();
 
-    const datasources = getCallToolResult(toolExecutions[0], z.array(dataSourceSchema));
-    expect(datasources.length).greaterThan(0);
-    const datasource = datasources.find(
-      (datasource) => datasource.name === 'Superstore Datasource',
-    );
+    expect(toolExecutions[0].name).toBe('get_datasource_metadata');
+    expect(toolExecutions[0].arguments).toEqual({ datasourceLuid: superstore.id });
 
-    expect(datasource).toMatchObject({
-      id: superstore.id,
-      name: 'Superstore Datasource',
-    });
+    const { fields } = getCallToolResult(toolExecutions[0], fieldsResultSchema);
+    expect(fields.length).toBeGreaterThan(0);
+
+    const fieldNames = fields.map((field) => field.name);
+    expect(fieldNames).toContain('Postal Code');
+    expect(fieldNames).toContain('Product Name');
   });
 });
