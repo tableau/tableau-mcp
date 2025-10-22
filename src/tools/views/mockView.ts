ce1f3daf0841786ffd59b800fe94aa383eda73a1
Andy Young <andrew.young@tableau.com>
2025-10-22 15:20:58 -0700

    Add list-views test

--- src/tools/views/mockView.ts
+++ src/tools/views/mockView.ts
@@ -1,10 +1,19 @@
 export const mockView = {
   id: '4d18c547-bbb1-4187-ae5a-7f78b35adf2d',
   name: 'Overview',
+  createdAt: '2024-06-10T23:23:23Z',
+  updatedAt: '2024-06-10T23:23:23Z',
   project: {
     id: 'ae5e9374-2a58-40ab-93e4-a2fd1b07cf7d',
   },
   workbook: {
     id: '96a43833-27db-40b6-aa80-751efc776b9a',
+  },
+  tags: {
+    tag: [
+      {
+        label: 'tag-1',
+      },
+    ],
   },
 };
