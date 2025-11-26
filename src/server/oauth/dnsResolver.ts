497861861122d163e68a8dd594c291510bd04213
Andy Young <andrew.young@tableau.com>
2025-11-26 11:32:02 -0800

    Make DNS resolver lazy

--- src/server/oauth/dnsResolver.ts
+++ src/server/oauth/dnsResolver.ts
@@ -1,8 +1,12 @@
 import { Resolver } from 'dns/promises';
 
-const dnsResolver = new Resolver();
-dnsResolver.setServers(['1.1.1.1']); // Use Cloudflare public DNS
+let dnsResolver: Resolver | null = null;
 
 export function getDnsResolver(): Resolver {
+  if (!dnsResolver) {
+    dnsResolver = new Resolver();
+    dnsResolver.setServers(['1.1.1.1']); // Use Cloudflare public DNS
+  }
+
   return dnsResolver;
 }
