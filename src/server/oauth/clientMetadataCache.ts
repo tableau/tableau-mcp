318ab930da45f5821d455a87139a542f63b0d37a
Andy Young <andrew.young@tableau.com>
2025-12-02 11:09:27 -0800

    Use const

--- src/server/oauth/clientMetadataCache.ts
+++ src/server/oauth/clientMetadataCache.ts
@@ -1,7 +1,6 @@
+import { TEN_MINUTES_IN_MS } from '../../config.js';
 import { ExpiringMap } from '../../utils/expiringMap.js';
 import { ClientMetadata } from './schemas.js';
-
-const TEN_MINUTES_IN_MS = 1000 * 60 * 10;
 
 export const clientMetadataCache = new ExpiringMap<string, ClientMetadata>({
   defaultExpirationTimeMs: TEN_MINUTES_IN_MS,
