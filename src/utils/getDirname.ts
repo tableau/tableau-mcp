fc32f69010cb75ea516c2b4c23550829767d2018
Andy Young <andrew.young@tableau.com>
2025-12-08 17:33:35 -0800

    Resolve compiler errors

--- src/utils/getDirname.ts
+++ src/utils/getDirname.ts
@@ -1,13 +1,7 @@
-import { fileURLToPath } from 'url';
-
 export function getDirname(): string {
   if (typeof __dirname === 'string') {
     return __dirname;
   }
 
-  if (typeof import.meta !== 'undefined') {
-    return fileURLToPath(new URL('.', import.meta.url));
-  }
-
-  throw new Error('Unable to determine directory path: neither __dirname nor import.meta is available');
+  throw new Error('__dirname is not set');
 }
