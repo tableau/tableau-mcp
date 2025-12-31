a4ce2638bf4a1154a59dd879b5e59053bb09665a
Joe Constantino <joe.constantino3@gmail.com>
2025-12-31 13:39:55 -0800

    added content_url field to the response schema of the list_datasources tool. this field is the required value for the dbname field in the generateworkbook xml tool which generates the workbook xml template with the required properties that the new file_upload api expects.

--- src/tools/listDatasources/mockDatasources.ts
+++ src/tools/listDatasources/mockDatasources.ts
@@ -4,6 +4,7 @@ const datasources = [
   {
     id: '2d935df8-fe7e-4fd8-bb14-35eb4ba31d45',
     name: 'Superstore Datasource',
+    contentUrl: 'SuperstoreDatasource_12345678901234',
     project: {
       id: 'cbec32db-a4a2-4308-b5f0-4fc67322f359',
       name: 'Samples',
@@ -12,6 +13,7 @@ const datasources = [
   {
     id: 'ba1da5d9-e92b-4ff2-ad91-4238265d877c',
     name: 'Finance Datasource',
+    contentUrl: 'FinanceDatasource_23456789012345',
     project: {
       name: 'Finance',
       id: '4862efd9-3c24-4053-ae1f-18caf18b6ffe',
@@ -20,6 +22,7 @@ const datasources = [
   {
     id: 'a6fc3c9f-4f40-4906-8db0-ac70c5fb5a11',
     name: 'Sales Datasource',
+    contentUrl: 'SalesDatasource_34567890123456',
     project: {
       name: 'Finance',
       id: '4862efd9-3c24-4053-ae1f-18caf18b6ffe',
