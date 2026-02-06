2c67326aee7b7baf5e95a294bb26c98e14fff5dd
Stephen Montes De Oca <s.montesdeoca@salesforce.com>
2026-02-05 16:13:54 -0800

    more calc info

--- src/tools/contextProvider/getCalculationInfo.ts
+++ src/tools/contextProvider/getCalculationInfo.ts
@@ -86,12 +86,43 @@ export const getGetCalculationInfoTool = (server: Server): Tool<typeof paramsSch
   return getCalculationInfoTool;
 };
 
-const basicCalculationInfo = `
-TODO
+const basicCalculationExamples = `
+Example calculations and what they do:
+
+`;
+
+const lodCalculationExamples = `
+Example calculations and what they do:
+
 `;
 
 const lodCalculationInfo = `
-TODO
+LOD calculations compute values at the data source level and the visualization level, at a more granular level (INCLUDE), a less granular level (EXCLUDE), or an entirely independent level of detail (FIXED).
+
+LOD syntax: { [FIXED | INCLUDE | EXCLUDE] <dimension declaration> : <aggregate expression> }
+
+FIXED:
+- computes values using the specified dimensions without reference to the view level of detail.
+- ignores all the filters in the view other than context filters, data source filters, and extract filters.
+- Exmaple: { FIXED [Region] : SUM([Sales]) } computes the sum of Sales per region.
+
+INCLUDE:
+- computes values using the specified dimensions in addition to whatever dimensions are in the view.
+- are more useful when including dimensions that are not in the view.
+- Example: { INCLUDE [Customer Name] : SUM([Sales]) } computes the total sales
+
+EXCLUDE:
+- explicitly remove dimensions from the expression and are more useful when excluding dimensions that are in the view.
+- Example: { EXCLUDE [Region]: SUM([Sales]) }
+
+<dimension declaration>:
+- Specifies one or more dimensions that set the scope of the aggregate expression, according to the keyword. Use commas to separate multiple dimensions.
+- Example: [Name] or [Segment], [Category], [Region]
+- You can use any expression that evaluates as dimension, including Date expressions.
+- Example: {FIXED YEAR([Order Date]) : SUM([Sales])} aggregates the sum of Sales at the Year level.
+
+<aggregate expression>:
+- The aggregate expression is the calculation that is performed.
 `;
 
 const tableCalculationInfo = `
@@ -108,7 +139,11 @@ Calculations are composed of the following components:
 
 Fields are dimensions or measures from the data source. Fields are inserted into calculations by enclosing the field name in brackets. Example: [Sales Category].
 
-Constants are fixed values 
+Constants are fixed values used in calculations. The syntax for constants depends on the data type:
+- Strings use signle or double quotes. Example: "Sales" or 'Sales'
+- Numbers / Decimals are written as is. Example: 1000, 1.25
+- Booleans are written as true or false (lowercase).
+- Dates are wrapped by the pound symbol (#). Example: #August 22, 2005# or #2005-08-22#
 
 Parameter are placeholder values that can be inserted into calculations to replace constant values. Parameters are enclosed in square brackets. Example: [Selected Year].
 
@@ -130,6 +165,4 @@ Operator precedence:
 Parentheses can be used as needed to force an order of precedence. Operators that appear within parentheses are evaluated before those outside the parentheses, starting from the innermost parentheses and moving outward.
 
 Calculations do not always need to contain all components.
-
-Example Calculations and what they do:
 `;
