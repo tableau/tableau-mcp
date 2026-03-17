2aa3492a062bd847865127fa237bcd6fc135ea4e
Andy Young <andrew.young@tableau.com>
2026-03-16 17:43:55 -0700

    Fix typo

--- src/server/passthroughAuthMiddleware.test.ts
+++ src/server/passthroughAuthMiddleware.test.ts
@@ -11,7 +11,7 @@ describe('passthroughAuthMiddleware', () => {
       toolsWithoutMcpScopes,
       [
         'This test is designed to fail the first time a tool is added that does not require API scopes.',
-        'If you see this error, and your tool indeed requires no API scopes,you must add the appropriate logic to prevent calling the tool with passthrough auth.',
+        'If you see this error, and your tool indeed requires no API scopes, you must add the appropriate logic to prevent calling the tool with passthrough auth.',
         'See: https://github.com/tableau/tableau-mcp/pull/241/changes#r2942474421',
       ].join('\n'),
     ).toHaveLength(0);
