52f133ac3b540f9094ee745e2713499733a6f81e
Andy Young <andrew.young@tableau.com>
2026-03-16 17:42:35 -0700

    Add test to disallow passthrough auth for tools with no API scopes

---
+++ src/server/passthroughAuthMiddleware.test.ts
@@ -0,0 +1,19 @@
+import { toolNames } from '../tools/toolName';
+import { getRequiredApiScopesForTool } from './oauth/scopes';
+
+describe('passthroughAuthMiddleware', () => {
+  it('disallow passthrough auth when calling a tool without API scopes ', () => {
+    const toolsWithoutMcpScopes = toolNames.filter(
+      (tool) => getRequiredApiScopesForTool(tool).length === 0,
+    );
+
+    expect(
+      toolsWithoutMcpScopes,
+      [
+        'This test is designed to fail the first time a tool is added that does not require API scopes.',
+        'If you see this error, and your tool indeed requires no API scopes,you must add the appropriate logic to prevent calling the tool with passthrough auth.',
+        'See: https://github.com/tableau/tableau-mcp/pull/241/changes#r2942474421',
+      ].join('\n'),
+    ).toHaveLength(0);
+  });
+});
