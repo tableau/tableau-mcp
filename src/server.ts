61496d359c7e59a589affb61821e8f7b21530821
Andy Young <andrew.young@tableau.com>
2026-04-24 15:18:56 -0700

    Remove esbuild define

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { InitializeRequest, SetLevelRequestSchema } from '@modelcontextprotocol/sdk/types.js';
 
-import pkg from '../package.json';
 import { setNotificationLevel } from './logging/notification.js';
 import { TableauAuthInfo } from './server/oauth/schemas.js';
-
-export const serverName = 'tableau-mcp';
-export const serverVersion = pkg.version;
-export const userAgent = `${serverName}/${serverVersion}`;
 
 export type ClientInfo = InitializeRequest['params']['clientInfo'];
 
@@ -30,7 +25,17 @@ export abstract class Server {
     return this._clientInfo ?? this.mcpServer.server.getClientVersion();
   }
 
-  constructor({ mcpServer, clientInfo }: { mcpServer?: McpServer; clientInfo?: ClientInfo } = {}) {
+  constructor({
+    mcpServer,
+    clientInfo,
+    serverName,
+    serverVersion,
+  }: {
+    mcpServer?: McpServer;
+    clientInfo?: ClientInfo;
+    serverName: string;
+    serverVersion: string;
+  }) {
     this.mcpServer =
       mcpServer ??
       new McpServer(
@@ -49,6 +54,17 @@ export abstract class Server {
     this.name = serverName;
     this.version = serverVersion;
     this._clientInfo = clientInfo;
+  }
+
+  get userAgent(): string {
+    const userAgentParts = [`${this.name}/${this.version}`];
+    if (this.clientInfo) {
+      const { name, version } = this.clientInfo;
+      if (name) {
+        userAgentParts.push(version ? `(${name} ${version})` : `(${name})`);
+      }
+    }
+    return userAgentParts.join(' ');
   }
 
   abstract registerTools: (tableauAuthInfo?: TableauAuthInfo) => Promise<void>;
