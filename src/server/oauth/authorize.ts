1f88dd7c4bee949fff2a69210bc1a18c3f29d70f
Andy Young <andrew.young@tableau.com>
2026-05-06 10:46:18 -0700

    Add milliseconds methods

--- src/server/oauth/authorize.ts
+++ src/server/oauth/authorize.ts
@@ -5,7 +5,8 @@ import { isSSRFSafeURL } from 'ssrfcheck';
 import { Err, Ok, Result } from 'ts-results-es';
 import { fromError } from 'zod-validation-error/v3';
 
-import { getConfig, ONE_DAY_IN_MS } from '../../config.js';
+import { getConfig } from '../../config.js';
+import { milliseconds } from '../../milliseconds.js';
 import { axios, AxiosResponse, getStringResponseHeader, isAxiosError } from '../../utils/axios.js';
 import { parseUrl } from '../../utils/parseUrl.js';
 import { retry } from '../../utils/retry.js';
@@ -347,7 +348,10 @@ async function getClientFromMetadataDoc(
   if (cacheControlMaxAge) {
     const cacheControlMaxAgeSeconds = parseInt(cacheControlMaxAge);
     if (!isNaN(cacheControlMaxAgeSeconds) && cacheControlMaxAgeSeconds >= 0) {
-      cacheExpiryMs = Math.min(ONE_DAY_IN_MS, cacheControlMaxAgeSeconds * 1000);
+      cacheExpiryMs = Math.min(
+        milliseconds.fromDays(1),
+        milliseconds.fromSeconds(cacheControlMaxAgeSeconds),
+      );
     }
   }
 
--- src/server/oauth/clientMetadataCache.ts
+++ src/server/oauth/clientMetadataCache.ts
@@ -1,7 +1,7 @@
-import { TEN_MINUTES_IN_MS } from '../../config.js';
+import { milliseconds } from '../../milliseconds.js';
 import { ExpiringMap } from '../../utils/expiringMap.js';
 import { ClientMetadata } from './schemas.js';
 
 export const clientMetadataCache = new ExpiringMap<string, ClientMetadata>({
-  defaultExpirationTimeMs: TEN_MINUTES_IN_MS,
+  defaultExpirationTimeMs: milliseconds.fromMinutes(10),
 });
