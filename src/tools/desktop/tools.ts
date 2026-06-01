505a5c31c77c651448168a7780cdff0b31fb54cd
Andy Young <andrew.young@tableau.com>
2026-06-01 16:48:24 -0700

    Register getApplyWorkbookTool

--- src/tools/desktop/tools.ts
+++ src/tools/desktop/tools.ts
@@ -1,9 +1,11 @@
 import { getCheckForUserChangesTool } from './session/checkForUserChanges.js';
 import { getListInstancesTool } from './session/listInstances.js';
+import { getApplyWorkbookTool } from './workbook/applyWorkbook.js';
 import { getGetWorkbookXmlTool } from './workbook/getWorkbookXml.js';
 
 export const desktopToolFactories = [
   getListInstancesTool,
   getCheckForUserChangesTool,
   getGetWorkbookXmlTool,
+  getApplyWorkbookTool,
 ];
