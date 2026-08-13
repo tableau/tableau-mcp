c8fab2514d18fe556fae98467245e757c7027b46
jarhun88 <j.song@salesforce.com>
2026-08-13 15:19:02 -0400

    Fix staged upload CI checks

--- tests/e2e/fixtures/uploadValidatePublishFeatureGate.cjs
+++ tests/e2e/fixtures/uploadValidatePublishFeatureGate.cjs
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
 class UploadValidatePublishFeatureGate {
   async isFeatureEnabled(featureName) {
     return featureName === 'upload-validate-publish';
