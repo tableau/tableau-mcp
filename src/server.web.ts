0a8573fc9305cc393ff70339d77061f47466badf
Stephen Montes De Oca <s.montesdeoca@salesforce.com>
2026-09-09 18:11:34 -0700

    Fix lint

--- src/server.web.ts
+++ src/server.web.ts
@@ -100,10 +100,6 @@ export function buildWebInstructions(): string {
   return adminToolsEnabled ? `${BASE_INSTRUCTIONS} ${ADMIN_INSTRUCTIONS}` : BASE_INSTRUCTIONS;
 }
 
-type RegistrationContext = {
-  siteRole?: string;
-};
-
 export class WebMcpServer extends Server {
   constructor({
     mcpServer,
